5760932c1246accecb98658388893106c86d4fe4
Adham <muzaffarov.adham@gmail.com>
2021-08-07 11:38:46 +0500

    db schema ended

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -1,13 +1,48 @@
-CREATE DATABASE mvc;
+CREATE DATABASE blog;
+CREATE EXTENSION pgcrypto;
 
-CREATE TABLE users(
-    id serial primary key,
-    firstname varchar(32) not null,
-    lastname varchar(64) not null,
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE posts(
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    image VARCHAR(50) NOT NULL,
+    category_id INTEGER REFERENCES categories(id) ON DELETE SET NULL,
+    content TEXT NOT NULL,
     date timestamptz default current_timestamp
 );
 
-INSERT INTO users(firstname, lastname) VALUES ('Adham', 'Muzaffarov');
+CREATE TABLE admins (
+    id  SERIAL PRIMARY KEY,
+    username VARCHAR(20) NOT NULL,
+    password VARCHAR(250) NOT NULL
+);
 
-SELECT * FROM users;
+INSERT INTO categories (
+    name
+)
+VALUES
+('Coding'),
+('SEO');
+
+INSERT INTO posts (
+    title,
+    image,
+    category_id,
+    content
+) 
+VALUES 
+('My first blog post', 'blog.jpg', 1, 'This is my first blog post...');
+
+INSERT INTO admins (
+    username,
+    password
+)
+VALUES
+('Adham', crypt('heaven_8', gen_salt('bf')));
+
+SELECT * FROM admins WHERE username = 'Adham' AND password = crypt('heaven_8', password);
 
